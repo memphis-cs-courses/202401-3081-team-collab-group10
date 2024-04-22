38294071dc443dd7bdeae01df19a7086ccfd0e42
Dalton Mann <daltonjamann@gmail.com>
2024-04-22 14:43:26 -0500

    Added daltonmann page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get 'daltonmann', to: 'pages#daltonmann', as: 'daltonmann'
 end
