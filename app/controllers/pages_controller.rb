6060b41e2ab2c117310d551096eed61170d37fec
Vivian Ly <viv.ly.2803@gmail.com>
2024-04-22 15:05:37 -0500

    Add viv-vos page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def viv-vos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+  get 'viv-vos', to: 'pages#viv-vos', as: 'viv-vos'
 end
